1dba8e7d5981e552412cd5541f292e06cda06408
Oscar Munoz <osca.munozs@duocuc.cl>
2025-10-01 03:00:51 -0300

    feat: se agregan estilos responsivos

--- src/app/core/pages/products-dashboard/products-dashboard.component.ts
+++ src/app/core/pages/products-dashboard/products-dashboard.component.ts
@@ -1,5 +1,6 @@
-import { Component, inject } from '@angular/core';
+import { Component, HostListener, inject } from '@angular/core';
 import { faWhatsapp } from '@fortawesome/free-brands-svg-icons';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { AddProductModalComponent } from '../../components/add-product-modal/add-product-modal.component';
 
@@ -14,9 +15,28 @@ export class ProductsDashboardComponent {
   // === Estado ====
   // Icons
   faWhatsapp = faWhatsapp;
+  faBars = faBars;
+  isMenuOpen = false;
 
   // === Métodos Públicos ===
   public openAddProductModal() {
     const modalRef = this._modalService.open(AddProductModalComponent);
+  }
+
+  public toggleMenu(): void {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
+
+  public onMenuItemClick(): void {
+    if (window.innerWidth >= 768 && window.innerWidth < 1200) {
+      this.isMenuOpen = false;
+    }
+  }
+
+  @HostListener('window:resize')
+  public handleResize(): void {
+    if (window.innerWidth >= 1200 && this.isMenuOpen) {
+      this.isMenuOpen = false;
+    }
   }
 }
